0fc6e34a3899ff014a7c10b60c192d81233f368e
Student <dervish17@yandex.ru>
2025-01-24 20:56:00 +0900

    Главное окно

--- app/mainWin.py
+++ app/mainWin.py
@@ -1,7 +1,8 @@
-from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QWidget, QLabel, QMainWindow, QPushButton, QVBoxLayout,
                              QHBoxLayout, QApplication)
-from PyQt6.QtGui import QIcon, QPixmap
+from PyQt6.QtGui import QIcon, QPixmap, QFont
+from app.authWindow import AuthWindow
+from app.viewUser import ViewUser
 
 
 class MainWindow(QMainWindow):
@@ -10,26 +11,22 @@ class MainWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        self.setStyleSheet("background-color: #90e6e4;")
-        self.setWindowTitle('Фотостудия "Фото"')
+        self.setStyleSheet("background-color: #000000;")
+        self.setWindowTitle('Фотостудия "Narcissus"')
         self.resize(300, 400)
-        self.setWindowIcon(QIcon('resources/54544.png'))
+        self.setWindowIcon(QIcon('resources/12.png'))
 
-        picture_label = QLabel()
-        pixmap = QPixmap('resources/54544.png')
-        picture_label.setPixmap(pixmap)
+        picture_label = QLabel('Фотостудия ')
+        label = QLabel('Narcissus')
+        picture_label.setStyleSheet("QLabel {font: 24pt Monotype Corsiva; color: white; font-weight: bold;}")
+        label.setStyleSheet("QLabel {font: 30pt Embassy BT; color: pink; font-weight: bold;}")
 
         central_widget = QWidget()
 
-        label = QLabel('Войти как')
-        label.setStyleSheet("QLabel {font: 24pt Arial; color: black; font-weight: bold;}")
-        self.admin_btn = QPushButton('Администратор')
-        self.admin_btn.setFixedSize(150, 50)
         self.user_btn = QPushButton('Пользователь')
-        self.user_btn.setFixedSize(150, 50)
-
-        button_style = """QPushButton {
-                                font-size: 17px; 
+        button_style = """
+                            QPushButton {
+                                font-size: 16px; 
                                 background-color: white; 
                                 color: black; 
                                 border: none; 
@@ -41,15 +38,18 @@ class MainWindow(QMainWindow):
                             }
                             QPushButton:pressed {
                                 background-color: #3c3c3c;
-                            }"""
-        self.admin_btn.setStyleSheet(button_style)
+                            }
+                        """
         self.user_btn.setStyleSheet(button_style)
+        self.user_btn.setFont(QFont('Monotype Corsiva'))
+        self.admin_btn = QPushButton('Администратор')
+        self.admin_btn.setStyleSheet('QPushButton {font: 15pt Monotype Corsiva; color: grey; padding: 8px; '
+                                     'border-radius: 5px;}')
+
 
         main_l = QVBoxLayout()
         h_l1 = QHBoxLayout()
         h_l2 = QHBoxLayout()
-        h_l3 = QHBoxLayout()
-        h_l4 = QHBoxLayout()
 
         h_l1.addStretch(2)
         h_l1.addWidget(label, 1)
@@ -57,37 +57,27 @@ class MainWindow(QMainWindow):
         h_l2.addStretch()
         h_l2.addWidget(picture_label)
         h_l2.addStretch()
-        h_l3.addStretch(1)
-        h_l3.addWidget(self.admin_btn)
-        h_l3.addStretch(1)
-        h_l4.addStretch(1)
-        h_l4.addWidget(self.user_btn)
-        h_l4.addStretch(1)
 
         main_l.addStretch()
         main_l.addLayout(h_l2)
         main_l.addLayout(h_l1)
-        main_l.addLayout(h_l3)
-        main_l.addLayout(h_l4)
+        main_l.addWidget(self.user_btn)
         main_l.addStretch()
+        main_l.addWidget(self.admin_btn)
 
         central_widget.setLayout(main_l)
         self.setCentralWidget(central_widget)
 
-        # self.admin_btn.clicked.connect(self.show_admin_auth)
-        # self.user_btn.clicked.connect(self.show_win_user)
+        self.admin_btn.clicked.connect(self.show_admin_auth)
+        self.user_btn.clicked.connect(self.show_win_user)
 
-    # def show_admin_auth(self):
-    #     self.auth = AuthWindow()
-    #     self.auth.show()
+    def show_admin_auth(self):
+        self.auth = AuthWindow()
+        self.auth.show()
 
-    # def show_win_user(self):
-    #     self.show_user = ViewCinemaUser()
-    #     self.show_user.show()
+    def show_win_user(self):
+        self.show_user = ViewUser()
+        self.show_user.show()
 
     def closeEvent(self, event):
         QApplication.quit()
-
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key.Key_Escape:
-            self.close()
---
+++ app/viewUser.py
@@ -0,0 +1,69 @@
+from PyQt6.QtGui import QIcon, QPixmap
+from PyQt6.QtWidgets import (QWidget, QPushButton, QVBoxLayout, QLabel, QHBoxLayout, QMessageBox)
+
+
+class ViewUser(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setStyleSheet("background-color: #000000;")
+        self.setWindowTitle('Выбор фильма')
+        self.resize(800, 800)
+        self.setWindowIcon(QIcon('resources/12.png'))
+
+        main_layout = QVBoxLayout()
+
+        photos_layout = QHBoxLayout()
+
+        photo_paths = [
+            "resources/1.jpg",
+            "resources/2.jpg",
+            "resources/3.jpg",
+            "resources/4.jpg",
+            "resources/5.jpg",
+        ]
+
+        for path in photo_paths:
+            item_widget = QWidget()
+            item_layout = QVBoxLayout(item_widget)
+
+            image_label = QLabel()
+            pixmap = QPixmap(path)
+            pixmap = pixmap.scaled(250, 400)
+            image_label.setPixmap(pixmap)
+
+            button = QPushButton("Забронировать")
+            button.setStyleSheet("""
+                QPushButton {
+                    font-size: 16px;
+                    background-color: white;
+                    color: black;
+                    padding: 10px;
+                    border-radius: 5px;
+                }
+                QPushButton:hover {
+                    background-color: grey;
+                }
+                QPushButton:pressed {
+                    background-color: #3c3c3c;
+                }
+            """)
+            button.clicked.connect(lambda _, p=path: self.book_photo(p))
+
+            item_layout.addWidget(image_label)
+            item_layout.addWidget(button)
+            photos_layout.addWidget(item_widget)
+
+        main_layout.addLayout(photos_layout)
+        self.setLayout(main_layout)
+
+    def book_photo(self, photo_path):
+        msg_box = QMessageBox()
+        msg_box.setWindowTitle("Бронирование")
+        msg_box.setText(f"Вы выбрали фотографию: {photo_path}")
+        msg_box.setIcon(QMessageBox.Icon.Information)
+        msg_box.setStandardButtons(QMessageBox.StandardButton.Ok)
+        msg_box.setStyleSheet("background-color: white; color: black;")
+        msg_box.exec()
